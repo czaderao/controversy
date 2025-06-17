bd9fed43de229a27ec364472a176bb0ddbf0a353
czaderao <ondrej.czadera@gmail.com>
2025-06-17 14:24:37 +0200

    Minor tweaks in main.py; now processes one file if run through the terminal

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
 from flagger import process_csv
+from merge_res import patch_missing_geocodes
 from ri_data_geocoder import semgis_geocode
 
-# work in progress
+# run for a single file processing. Input filename as just FILENAME without the .csv; ex. python3 main.py RI_all
 if __name__ == '__main__':
-    process_csv(sys.argv[1])
-    semgis_geocode(sys.argv[1])
+    process_csv(f'{sys.argv[1]}')
+    semgis_geocode(f'{sys.argv[1]}')
+    patch_missing_geocodes(f'{sys.argv[1]}')
+    print(f'File {sys.argv[1]} has been processed')
